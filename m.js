f49bbc943b395edd545449b0e00713abc9274728
Anele <anele@projectcodex.co>
2018-01-17 11:56:58 +0200

    displaying the api on the front end

---
+++ m.js
@@ -0,0 +1,23 @@
+(function() {
+  function initTemplate(templateSelector) {
+    var templateText = document.querySelector(templateSelector).innerHTML;
+    return Handlebars.compile(templateText);
+  }
+
+  var divsTemp = initTemplate('.divsTemp')
+  var moviesDetailsHtml = document.querySelector('.moviesDetails');
+
+
+  var getAllMovies = function() {
+    var url =
+      "https://api.themoviedb.org/4/list/1?api_key=7e719bfe3cd3786ebf0a05d3b138853d";
+    $.get(url)
+      .then(function(movies) {
+        console.log(movies);
+        moviesDetailsHtml.innerHTML = divsTemp({
+          moviesDetails: movies.results
+        })
+      });
+  };
+  getAllMovies();
+})();
