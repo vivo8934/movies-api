72d18d915c99a7c2c533bb14fb7bd002d7af34a2
Anele <anele@projectcodex.co>
2018-01-18 12:26:46 +0200

    starting server side

--- public/movies.js
+++ public/movies.js
@@ -1,5 +1,5 @@
 var moviesDetailsHtml = document.querySelector('.moviesDetails');
-  var divsTemp = initTemplate('.divsTemp')
+  var divsTemp = initTemplate('.divsTemp');
 
   function initTemplate(templateSelector) {
     var templateText = document.querySelector(templateSelector).innerHTML;
@@ -19,25 +19,16 @@ var moviesDetailsHtml = document.querySelector('.moviesDetails');
       });
   };
 
-
-
-
-
   getAllMovies();
 })();
 
-var MyMovies = [];
 var search = document.querySelector(".mySearch");
 search.addEventListener('keyup',  function(evt){
   var searchText = evt.target.value;
-  // if (searchText.length < 3){
-  //   return;
-  // };
   var url =
     "https://api.themoviedb.org/3/search/movie?api_key=7e719bfe3cd3786ebf0a05d3b138853d&query=" + searchText;
     $.get(url)
 .then(function(filteredMovies){
-
   console.log(filteredMovies);
  moviesDetailsHtml.innerHTML = divsTemp({
    moviesDetails: filteredMovies.results
